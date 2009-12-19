a37b8a6623fd460107a6eae79b3d7e6830c164f7
Dan Parks <dan@eastofcleveland.com>
2009-12-19 02:29:10 -0500

    started integrating flickr

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
 
   def home
+#    require 'flickr_fu'
+#    flickr = Flickr.new("#{RAILS_ROOT}/config/flickr.yml")
+#    @photos = flickr.photos.search(:tags => 'yoyocase')
+
+    @photos = []
   end
 
   def credits
