8514288a6c1336968ac6429fc4081d898d7252b5
Dan Parks <dan@eastofcleveland.com>
2009-12-22 01:32:38 -0500

    do not allow users to edit other user accounts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
   def update
-    @user = User.find(params[:id])
+    @user = current_user
     if @user.update_attributes(params[:user])
       flash[:msg] = "Preferences updated successfully"
       redirect_to account_url
