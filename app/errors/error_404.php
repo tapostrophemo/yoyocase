66441dd16c94051c3d7b4d73933ae4db3dd68110
Dan Parks <dan@eastofcleveland.com>
2010-02-13 11:49:08 -0500

    put site template in 404 page

--- app/errors/error_404.php
+++ app/errors/error_404.php
@@ -1,34 +1,33 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title>404 Page Not Found</title>
-<style type="text/css">
-
-body {
-background-color:	#fff;
-margin:				40px;
-font-family:		Lucida Grande, Verdana, Sans-serif;
-font-size:			12px;
-color:				#000;
-}
-
-#content  {
-border:				#999 1px solid;
-background-color:	#fff;
-padding:			20px 20px 12px 20px;
-}
-
-h1 {
-font-weight:		normal;
-font-size:			14px;
-color:				#990000;
-margin: 			0 0 4px 0;
-}
-</style>
+<title>yoyocase.net : 404 page not found</title>
+<link rel="stylesheet" type="text/css" href="/res/main.css"/>
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
 </head>
 <body>
-	<div id="content">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
+<div id="container">
+
+<div id="header">
+ <h1><a href="/">yoyocase.net</a></h1>
+</div>
+
+<div id="content">
+ <div class="err">
+  <h1><?php echo $heading; ?></h1>
+  <?php echo $message; ?>
+ </div>
+</div><!-- /#content -->
+
+<div id="footer">
+ Copyright &copy; 2009, 2010, Dan Parks. All Rights Reserved.<br/>
+ User submissions copyright the submitter. Other content derived from Creative Commons-licensed material.<br/>
+ <br/>
+ <a href="/credits">Credits</a> | <a href="/privacy">Privacy</a>
+</div>
+
+</div><!-- /#container -->
+
 </body>
 </html>
+
