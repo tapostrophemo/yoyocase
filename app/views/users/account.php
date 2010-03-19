4ed2479cc047f17c434d2646f6ec6a25d1ecf19a
Dan Parks <dan@eastofcleveland.com>
2010-03-18 23:59:46 -0400

    set max-width for images

--- app/views/users/account.php
+++ app/views/users/account.php
@@ -4,11 +4,11 @@
  <p>What do you want to do next?</p>
  <ul>
   <li>Manage your <a href="/yoyos">collection</a>?</li>
-  <li>View <a href="/yoyos/<?=$this->session->userdata('username')?>">your gallery</a>?</li>
   <li>Edit your <a href="/preferences">account preferences</a>?</li>
+  <li>View <a href="/yoyos/<?=$this->session->userdata('username')?>">your gallery</a>?</li>
   <li>View <a href="/galleries">all galleries</a>?</li>
  </ul>
- <p>Whatever you decide to do, be sure to <a href="/logout">logout</a> before you leave.</p>
+ <p>Whatever you decide to do, be sure to <a href="/logout">logout</a> when you leave.</p>
 </div>
 
 <div id="sidebar">
