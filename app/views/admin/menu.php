4a6d676bc80e04b6a657a5202ed128862977049d
Dan Parks <dan@eastofcleveland.com>
2010-09-29 01:39:29 -0400

    restoring "check pending thumbs" link everywhere

--- app/views/admin/menu.php
+++ app/views/admin/menu.php
@@ -3,8 +3,6 @@
 <ul>
  <li><a href="/admin/accounts">user accounts</a></li>
  <li><a href="/admin/registrationActivationReport">registration/activation report</a></li>
-<?php if ($this->input->server('SERVER_NAME') == 'dev.yoyocase.net'): ?>
  <li><a href="/admin/checkThumbnails">check thumbnails</a></li>
-<?php endif; ?>
 </ul>
 
