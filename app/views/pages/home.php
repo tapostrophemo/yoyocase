c9403f63bb38bf0c99345852556cc90b5ceaa2bd
Dan Parks <dan@eastofcleveland.com>
2010-10-17 21:37:29 -0400

    updated wording

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -2,7 +2,7 @@
 
 <div id="gallery">
 
-<p>What is yoyocase.net? It is a website that will let you manage your yoyo collection online.
+<p>What is yoyocase.net? It is a website that lets you manage your yoyo collection online.
  Managing your collection online means you'll be able to easily share your collection with
  friends, traders, and other yoyo enthusiasts.</p>
 
@@ -12,6 +12,8 @@
 <ul>
  <li>link pictures of your collection from flickr or Photobucket accounts</li>
  <li>post yoyo details (manufacturer, model name, notes, etc.)</li>
+ <li>include advanced details (cost, estimated value, condition, etc.)</li>
+ <li>view photo galleries of other users' collections</li>
 </ul>
 
 <br/>
@@ -19,9 +21,8 @@
 <p>Some of the features planned for yoyocase.net include:</p>
 <ul>
  <li>link pictures from other popular image hosting sites (imageshack, imgur, etc.)</li>
- <li>include advanced details (cost, estimated value, condition, etc.)</li>
  <li>keep track of gifts, purchases and trades (date, people/stores involved, etc.)</li>
- <li>view other users' galleries and keep in touch with other yoyo enthusiasts</li>
+ <li>keep in touch with other yoyo enthusiasts</li>
 </ul>
 
 <br/>
