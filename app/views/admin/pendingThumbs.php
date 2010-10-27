8b774b8346d932feea747f047a87f8124cdbf824
Dan Parks <dan@eastofcleveland.com>
2010-10-26 22:25:58 -0400

    visual tweaks on pending icons report

--- app/views/admin/pendingThumbs.php
+++ app/views/admin/pendingThumbs.php
@@ -3,20 +3,24 @@
 <?php if (count($photos)): ?>
 
 <table class="report">
- <tr>
-  <th>ID</th>
-  <th>Username</th>
-  <th>Yoyo</th>
-  <th>URL</th>
+ <thead>
+  <tr>
+   <th>ID</th>
+   <th>Username</th>
+   <th>Yoyo</th>
+   <th>URL</th>
  </tr>
-<?php foreach ($photos as $photo): ?>
- <tr>
-  <td><?=$photo->id?></td>
-  <td><?=$photo->username?></td>
-  <td><?=$photo->model_name?></td>
-  <td><?=htmlspecialchars($photo->url)?></td>
- </tr>
-<?php endforeach; ?>
+ </thead>
+ <tbody>
+ <?php foreach ($photos as $photo): ?>
+  <tr>
+   <td><?=$photo->id?></td>
+   <td><?=$photo->username?></td>
+   <td><?=$photo->model_name?></td>
+   <td><?=htmlspecialchars($photo->url)?></td>
+  </tr>
+ <?php endforeach; ?>
+ </tbody>
 </table>
 
 <!--
