f58a5144a0ce76f78f9988c8a2616bbc68213fb8
Dan Parks <dan@eastofcleveland.com>
2010-12-19 20:44:38 -0500

    upgraded CI to 1.7.3

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 
 error_reporting(E_ALL | E_STRICT);
 
-$system_folder = '../lib/ci-1.7.2'; // TODO: configure for PROD deploy
+$system_folder = '../lib/ci-1.7.3'; // TODO: configure for PROD deploy
 $application_folder = '../app'; // TODO: configure for PROD deploy
 
 if (strpos($system_folder, '/') === FALSE) {
