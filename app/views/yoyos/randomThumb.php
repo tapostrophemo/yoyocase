cdfda4b7d26533f91296b0fa8108ec1d2a5397a5
Dan Parks <dan@eastofcleveland.com>
2011-02-01 00:14:26 -0500

    tidying up the JavaScript

--- app/views/yoyos/randomThumb.php
+++ app/views/yoyos/randomThumb.php
@@ -1,27 +1,23 @@
 <div id="slideshow">
 <?php foreach ($this->Misc->randomThumbs() as $thumb): if (mt_rand(0, 42) > 41): ?>
- <img src="<?=site_url().'thumbs/'.$thumb?>" style="display:none"/>
+ <img src="<?=site_url().'thumbs/'.$thumb?>"/>
 <?php endif; endforeach; ?>
 </div>
 
 <script type="text/javascript">
-var index = 0;
-var slides = null;
-var time = 450;
-
-function playSlideshow() {
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[index++ % slides.length].style.display = "block";
+var playSlideshow = (function () {
+  var index = 0;
+  var slides = document.getElementById("slideshow").children;
+  var time = 450;
   setTimeout("playSlideshow()", time);
-}
 
-function startSlideshow() {
-  slides = document.getElementById("slideshow").children;
-  setTimeout("playSlideshow()", time);
-};
-
-startSlideshow();
+  return function () {
+    for (var i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[index++ % slides.length].style.display = "block";
+    setTimeout("playSlideshow()", time);
+  };
+})();
 </script>
 
