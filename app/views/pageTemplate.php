ff476c35bdb8f580f07ad1fa1c841fc52d9ae9d0
Dan Parks <dan@eastofcleveland.com>
2015-11-10 19:01:57 -0500

    getting it all to run w/stricter environment

--- app/views/pageTemplate.php
+++ app/views/pageTemplate.php
@@ -31,11 +31,11 @@
   <?php if ($this->session->userdata('is_admin')): echo '<li><a href="/admin" class="lsf-icon admin">site admin</a></li>'; endif; ?>
   <li><a href="/galleries" class="lsf-icon galleries">galleries</a></li>
   <li><a href="/logout" class="lsf-icon logout">logout</a></li>
- <? else: ?>
+ <?php else: ?>
   <li><a href="/register" class="lsf-icon register">register</a></li>
   <li><a href="/galleries" class="lsf-icon galleries">galleries</a></li>
   <li><a href="/login" class="lsf-icon login">login</a></li>
- <? endif; ?>
+ <?php endif; ?>
  </ul>
 </div>
 
